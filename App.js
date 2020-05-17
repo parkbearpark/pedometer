6bc9138392575ffb48dc65272fc015e3a432123b
yuma <yumai6205@gmail.com>
2020-05-17 23:00:04 +0900

    App

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import DisplaySteps from './src/components/DisplaySteps'
+import Home from './src/screens/HomeScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <DisplaySteps />
-    </View>
-  )
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFC9D2',
-  },
-})
